7daa8e739da8033dab185376738c4306b6fb866d
Castlebin <Castlebin@gmail.com>
2025-05-19 22:01:34 +0800

    positional embedding

--- my_code/ch02/2.5_GPT_dataset.py
+++ my_code/ch02/2.5_GPT_dataset.py
@@ -58,3 +58,31 @@ data_iter = iter(dataloader)  # A 将数据加载器转换为 Python 迭代器�
 first_batch = next(data_iter)
 print(first_batch)
 
+
+#%% Listing 2.8 Tokenize the text and create a data loader
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+max_length = 4
+dataloader = create_dataloader_v1(
+  	raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)
+
+
+#%% positional embedding
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print(pos_embeddings.shape)
+
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
+
